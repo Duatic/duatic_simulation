91734fca86709e561479d66b76873703e2e85ae3
mbloechli <mbloechli@student.ethz.ch>
2025-10-06 19:25:27 +0200

    Use ros_gz_bridge with param file, instead of hard-coding

--- launch/ros_gz_bridge.launch.py
+++ launch/ros_gz_bridge.launch.py
@@ -1,82 +1,64 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node, PushRosNamespace
+
+import tempfile
 
 ARGUMENTS = [
     DeclareLaunchArgument(
         "namespace", default_value="empty_namespace", description="Robot namespace"
     ),
-    DeclareLaunchArgument("world", default_value="unspecified", description="World name"),
+    DeclareLaunchArgument("world", default_value="undefined", description="World name"),
+    DeclareLaunchArgument("config_file", description="Path to the config file"),
 ]
 
 
-def generate_launch_description():
+def generate_bridge_params(namespace, world, config_file):
 
-    ros_gz_bridge = GroupAction(
-        [
-            PushRosNamespace(LaunchConfiguration("namespace")),
-            # TODO: Update to ROS Kilted will add support for custom URDF frames making this file unnecessary
-            # The new launchfile for models integrates the ros_gz_bridge https://github.com/gazebosim/ros_gz/blob/kilted/ros_gz_sim/launch/ros_gz_spawn_model.launch.py
-            # Depth Camera
-            # Lidar
-            Node(
-                package="ros_gz_bridge",
-                executable="parameter_bridge",
-                name="rplidar_bridge",
-                output="screen",
-                parameters=[{"use_sim_time": True}],
-                arguments=[
-                    [
-                        "/world/",
-                        LaunchConfiguration("world"),
-                        "/model/",
-                        LaunchConfiguration("namespace"),
-                        "/link/",
-                        "base_link/sensor/rplidar/scan"
-                        + "@sensor_msgs/msg/LaserScan"
-                        + "[gz.msgs.LaserScan",
-                    ],
-                    [
-                        "/world/",
-                        LaunchConfiguration("world"),
-                        "/model/",
-                        LaunchConfiguration("namespace"),
-                        "/link/",
-                        "base_link/sensor/rplidar/scan/points"
-                        + "@sensor_msgs/msg/PointCloud2"
-                        + "[gz.msgs.PointCloudPacked",
-                    ],
-                ],
-                remappings=[
-                    (
-                        [
-                            "/world/",
-                            LaunchConfiguration("world"),
-                            "/model/",
-                            LaunchConfiguration("namespace"),
-                            "/link/",
-                            "base_link/sensor/rplidar/scan",
-                        ],
-                        ["sensor/lidar/scan"],
-                    ),
-                    (
-                        [
-                            "/world/",
-                            LaunchConfiguration("world"),
-                            "/model/",
-                            LaunchConfiguration("namespace"),
-                            "/link/",
-                            "base_link/sensor/rplidar/scan/points",
-                        ],
-                        ["sensor/lidar/scan/points"],
-                    ),
-                ],
-            ),
-        ]
+    # Read the template file
+    with open(config_file) as f:
+        content = f.read()
+
+    # Replace placeholders
+    content = content.replace("<robot_namespace>", namespace)
+    content = content.replace("<world_namespace>", world)
+
+    # Write to a temp file
+    tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".yaml")
+    tmp_file.write(content.encode())
+    tmp_file.close()
+    return tmp_file.name
+
+
+# This function is called at launch-time with the LaunchContext
+def launch_setup(context, *args, **kwargs):
+    namespace = LaunchConfiguration("namespace").perform(context)
+    world = LaunchConfiguration("world").perform(context)
+    config_file = LaunchConfiguration("config_file").perform(context)
+
+    params_file = generate_bridge_params(namespace, world, config_file)
+
+    ros_gz_bridge = PushRosNamespace(namespace)
+
+    bridge_node = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "--ros-args",
+            "-p",
+            f"config_file:={params_file}",
+        ],
+        output="screen",
     )
 
-    # Create launch description and add actions
+    return [ros_gz_bridge, bridge_node]
+
+
+def generate_launch_description():
     ld = LaunchDescription(ARGUMENTS)
-    ld.add_action(ros_gz_bridge)
+
+    # Use OpaqueFunction to delay evaluation until runtime
+    ld.add_action(OpaqueFunction(function=launch_setup))
+
     return ld
